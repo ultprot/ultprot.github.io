0c0225091abfb830fa09a3433040869f5430d901
ultprot <ultprot@gmail.com>
2020-03-02 15:23:09 +0900

    곱셈 게시물 게시

--- temp.js
+++ temp.js
@@ -1,16 +1,40 @@
-const WHITE = Symbol();
-const RED = Symbol();
-const BLACK = Symbol();
+class Problem{
+    constructor(a, b, c){
+        this.a = BigInt(a);
+        this.b = Number(b);
+        this.c = BigInt(c);
+    }
 
-const color = WHITE;
-switch(color){
-    case WHITE:
-        console.log("WHITE");
-        break;
-    case RED:
-        console.log("RED");
-        break;
-    case BLACK:
-        console.log("BLACK");
-        break;
+    getAnswer(cb){
+        let temp = BigInt(0);
+        if(cb == 1){
+            temp = this.a;
+        }else{
+            temp = this.getAnswer(parseInt(cb/2));
+            temp *= temp;
+            temp %= this.c;
+            if(cb%2 !== 0){
+                temp *= (this.a % this.c);
+            }
+        }
+        return temp % this.c;
+    }
 }
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let a, b, c;
+
+rl.on("line", line=>{
+    const inputs = line.split(' ');
+    a = BigInt(inputs[0]);
+    b = Number(inputs[1]);
+    c = BigInt(inputs[2]);
+}).on("close", ()=>{
+    const problem = new Problem(a, b, c);
+    console.log(problem.getAnswer(b).toString(10));
+});
--- temp.py
+++ temp.py
@@ -1,13 +1,20 @@
-import enum
-class Color(enum.Enum):
-    WHITE = 0
-    RED = 1
-    BLACK = 2
+a, b, c = map(int, input().split(' '))
+class Problem:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def getAnswer(self, cb):
+        temp = 0
+        if(cb == 1):
+            temp = self.a
+        else:
+            temp = self.getAnswer(cb//2)
+            temp *= temp
+            temp %= self.c
+            if cb % 2 != 0:
+                temp *= (self.a % self.c)
+        return temp % self.c
 
-color = Color.WHITE
-if color == Color.WHITE:
-    print("WHITE")
-elif color == Color.RED:
-    print("RED")
-elif color == Color.BLACK:
-    print("BLACK")
+problem = Problem(a, b, c)
+print(problem.getAnswer(b))
