b379b716e4199778a2ddc55e28fed36504ae1258
ultprot <ultprot@gmail.com>
2020-04-13 23:30:29 +0900

    징검다리 게시물 게시

--- temp.js
+++ temp.js
@@ -1,49 +1,53 @@
 class Problem{
-    constructor(numbers){
-        this.numbers = numbers;
-        this.numbers.sort((a,b) => {
-            return a - b;
+    constructor(distance, rocks, n){
+        this.distance = distance;
+        this.rocks = rocks;
+        this.n = n;
+        this.rocks.sort((a,b)=>{
+            return a-b;
         });
     }
 
-    find(target){
-        let l = 0;
-        let r = this.numbers.length - 1
-        while(l<=r){
-            let m = parseInt((l+r)/2)
-            if(target === this.numbers[m]){
-                return true;
-            }else if(target<this.numbers[m]){
-                r = m - 1
+    isSatisfied(key){
+        let prev = 0;
+        let count = 0;
+        for(let i = 0;i<this.rocks.length;i++){
+            if(this.rocks[i] - prev < key){
+                count++;
             }else{
-                l = m + 1
+                prev = this.rocks[i];
             }
         }
-        return false;
+        if(this.distance - prev < key){
+            count++;
+        }
+        if(count>this.n){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    binarySearch(){
+        let l = 0;
+        let r = this.distance;
+        let answer = 0;
+        while(l<=r){
+            let mid = parseInt((l+r)/2);
+            if(this.isSatisfied(mid)){
+                answer = mid;
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+        return answer;
     }
 }
 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const lines = [];
-
-rl.on("line", line=>{
-    lines.push(line);
-}).on("close", ()=>{
-    const numbers = lines[1].split(' ').map(x => Number(x));
-    const targets = lines[3].split(' ').map(x => Number(x));
-
-    const problem = new Problem(numbers);
-
-    targets.map(x=>{
-        if(problem.find(x) === true){
-            console.log(1);
-        }else{
-            console.log(0);
-        }
-    })
-});
+function solution(distance, rocks, n){
+    var answer = 0;
+    const problem = new Problem(distance, rocks, n);
+    answer = problem.binarySearch();
+    return answer;
+}
--- temp.py
+++ temp.py
@@ -1,23 +1,40 @@
-from queue import PriorityQueue
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    
+class Problem:
+    def __init__(self, distance, rocks, n):
+        self.distance = distance
+        self.rocks = rocks
+        self.rocks.sort()
+        self.n = n
 
-person1 = Person("a", 10)
-person2 = Person("b", 20)
-person3 = Person("c", 30)
-person4 = Person("d", 40)
-person5 = Person("e", 50)
+    def isSatisfied(self, key):
+        prev = 0
+        count = 0
+        for rock in self.rocks:
+            if rock - prev < key:
+                count+=1
+            else:
+                prev = rock
+        if self.distance - prev < key:
+            count+=1
+        if count > self.n:
+            return False
+        else:
+            return True
 
-pq = PriorityQueue()
-
-pq.put((5, person1))
-pq.put((3, person3))
-pq.put((1, person5))
-pq.put((4, person2))
-pq.put((2, person4))
-
-while not pq.empty():
-    print(pq.get()[1].name)
+    def binarySearch(self):
+        l = 0
+        r = self.distance
+        answer = 0
+        while l<=r:
+            mid = (l+r)//2
+            if self.isSatisfied(mid):
+                answer = mid
+                l = mid+1
+            else:
+                r = mid-1
+        return answer
+        
+def solution(distance, rocks, n):
+    answer = 0
+    problem = Problem(distance, rocks, n)
+    answer = problem.binarySearch()
+    return answer
